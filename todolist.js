b860ec124cdca40e0e39fe6a15c0546ca9156688
elhamsaleh <elhamsaleh051@gmail.com>
2026-07-31 22:12:36 +0330

    added js code

---
+++ todolist.js
@@ -0,0 +1,35 @@
+idocument.addEventListener("DOMContentLoaded", () => {
+  const taskInput = document.getElementById("task-input");
+  const addTaskBtn = document.getElementById("add-task-btn");
+  const taskList = document.getElementById("task-list");
+  const emptyImage = document.querySelector(".empty-Image");
+  const toggleEmptyState = () => {
+    emptyImage.style.display =
+      taskList.children.length === 0 ? "block" : "none";
+  };
+  const addTask = (event) => {
+    event.preventDefault();
+    const taskText = taskInput.value.trim();
+    if (!taskText) {
+      return;
+    }
+    const li = document.createElement("li");
+    li.innerHTML = `
+    <input type="checkbox" class="checkbox">
+    <span>${taskText}</span> 
+    <div class="task-buttons">
+      <button class="edit-btn><i class=fa-solid fa-pen"></i></button>
+      <button class="delet-btn><i class=fa-solid fa-trash"></i></button>
+    </div>
+    `;
+    taskList.appendChild(li);
+    taskInput.value = "";
+    toggleEmptyState();
+  };
+  addTaskBtn.addEventListener("click", addTask);
+  taskInput.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      addTask(e);
+    }
+  });
+});
